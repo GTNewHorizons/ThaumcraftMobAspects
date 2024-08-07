21423d515d6ecd66c713bd680a918b98c3d513e0
dipo33 <mato.dipcar@gmail.com>
2024-08-07 23:44:36 +0200

    feat: remove hard deps on other tc aspect mods

--- src/main/java/iguanaman/thaumcraftmobaspects/PluginEnderZoo.java
+++ src/main/java/iguanaman/thaumcraftmobaspects/PluginEnderZoo.java
@@ -2,9 +2,6 @@ package iguanaman.thaumcraftmobaspects;
 
 import java.util.HashSet;
 
-import com.dreammaster.modfixes.GTpp.GregTechPlusPlusAbandonedAspectsFix;
-
-import fox.spiteful.forbidden.DarkAspects;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 
@@ -19,15 +16,14 @@ public class PluginEnderZoo implements AspectPlugin {
     public HashSet<ThaumcraftEntity> getThaumcraftMobs() {
         HashSet<ThaumcraftEntity> mobs = new HashSet<>();
 
-        mobs.add(new ThaumcraftEntity("enderzoo.DireWolf", new AspectList().add(Aspect.EARTH, 3).add(Aspect.BEAST, 3).add(DarkAspects.WRATH, 1)));
+        mobs.add(new ThaumcraftEntity("enderzoo.DireWolf", new AspectList().add(Aspect.EARTH, 3).add(Aspect.BEAST, 3)));
         mobs.add(new ThaumcraftEntity("enderzoo.WitherWitch", new AspectList().add(Aspect.MAN, 3).add(Aspect.POISON, 1).add(Aspect.MAGIC, 2).add(Aspect.DEATH, 1)));
         mobs.add(new ThaumcraftEntity("enderzoo.Enderminy", new AspectList().add(Aspect.ELDRITCH, 2).add(Aspect.AIR, 1).add(Aspect.TRAVEL, 1)));
-        mobs.add(new ThaumcraftEntity("enderzoo.FallenKnight", new AspectList().add(Aspect.UNDEAD, 2).add(Aspect.MAN, 1).add(Aspect.ARMOR, 1).add((Aspect) GregTechPlusPlusAbandonedAspectsFix.CUSTOMASPECTCOMPAT[4], 1)));
+        mobs.add(new ThaumcraftEntity("enderzoo.FallenKnight", new AspectList().add(Aspect.UNDEAD, 2).add(Aspect.MAN, 1).add(Aspect.ARMOR, 1).add(Aspect.ORDER, 1)));
         mobs.add(new ThaumcraftEntity("enderzoo.FallenMount", new AspectList().add(Aspect.BEAST, 4).add(Aspect.EARTH, 1).add(Aspect.UNDEAD, 2)));
         mobs.add(new ThaumcraftEntity("enderzoo.WitherCat", new AspectList().add(Aspect.BEAST, 1).add(Aspect.MIND, 1).add(Aspect.MOTION, 2).add(Aspect.SENSES, 2).add(Aspect.DEATH, 1)));
         mobs.add(new ThaumcraftEntity("enderzoo.ConcussionCreeper", new AspectList().add(Aspect.PLANT, 2).add(Aspect.FIRE, 2).add(Aspect.TRAVEL, 2)));
         mobs.add(new ThaumcraftEntity("enderzoo.DireSlime", new AspectList().add(Aspect.EARTH, 2).add(Aspect.LIFE, 1)));
-
 
         return mobs;
     }
--- src/main/java/iguanaman/thaumcraftmobaspects/PluginSpecialMobs.java
+++ src/main/java/iguanaman/thaumcraftmobaspects/PluginSpecialMobs.java
@@ -2,10 +2,6 @@ package iguanaman.thaumcraftmobaspects;
 
 import java.util.HashSet;
 
-import com.dreammaster.modfixes.GTpp.GregTechPlusPlusAbandonedAspectsFix;
-
-import fox.spiteful.forbidden.DarkAspects;
-import gregtech.api.enums.TC_Aspects;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 
@@ -32,7 +28,7 @@ public class PluginSpecialMobs implements AspectPlugin {
         mobs.add(new ThaumcraftEntity("SpecialMobs.EnderCreeper", creeperAspects.copy().add(Aspect.ELDRITCH, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.FireCreeper", creeperAspects.copy().add(Aspect.FIRE, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.GravelCreeper", creeperAspects.copy().add(Aspect.EARTH, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.GravityCreeper", creeperAspects.copy().add((Aspect) TC_Aspects.MAGNETO.mAspect, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.GravityCreeper", creeperAspects.copy().add(Aspect.TRAP, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.JumpingCreeper", creeperAspects.copy().add(Aspect.AIR, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.LightningCreeper", creeperAspects.copy().add(Aspect.WEATHER, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.MiniCreeper", new AspectList().add(Aspect.PLANT, 1).add(Aspect.FIRE, 1)));
@@ -46,7 +42,7 @@ public class PluginSpecialMobs implements AspectPlugin {
         mobs.add(new ThaumcraftEntity("SpecialMobs.IcyEnderman", endermanAspects.copy().add(Aspect.COLD, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.LightningEnderman", endermanAspects.copy().add(Aspect.WEATHER, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.MiniEnderman", new AspectList().add(Aspect.ELDRITCH, 2).add(Aspect.AIR, 1).add(Aspect.TRAVEL, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.MirageEnderman", endermanAspects.copy().add((Aspect) GregTechPlusPlusAbandonedAspectsFix.CUSTOMASPECTCOMPAT[1], 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.MirageEnderman", endermanAspects.copy().add(Aspect.ENTROPY, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.ThiefEnderman", endermanAspects.copy().add(Aspect.GREED, 1)));
 
         // GHASTS
@@ -55,9 +51,9 @@ public class PluginSpecialMobs implements AspectPlugin {
         mobs.add(new ThaumcraftEntity("SpecialMobs.BabyGhast", new AspectList().add(Aspect.SOUL, 1).add(Aspect.FIRE, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.FaintGhast", ghastAspects.copy().add(Aspect.AIR, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.FighterGhast", ghastAspects.copy().add(Aspect.WEAPON, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.KingGhast", ghastAspects.copy().add(Aspect.SOUL, 1).add(Aspect.FIRE, 1).add((Aspect) GregTechPlusPlusAbandonedAspectsFix.CUSTOMASPECTCOMPAT[4], 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.MiniGhast", new AspectList().add(Aspect.SOUL, 1).add(Aspect.FIRE, 1).add((Aspect) TC_Aspects.MAGNETO.mAspect, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.QueenGhast", ghastAspects.copy().add(Aspect.SOUL, 1).add(Aspect.FIRE, 1).add(DarkAspects.LUST, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.KingGhast", ghastAspects.copy().add(Aspect.SOUL, 1).add(Aspect.FIRE, 1).add(Aspect.MIND, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.MiniGhast", new AspectList().add(Aspect.SOUL, 1).add(Aspect.FIRE, 1).add(Aspect.TRAP, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.QueenGhast", ghastAspects.copy().add(Aspect.SOUL, 1).add(Aspect.FIRE, 1).add(Aspect.LIFE, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.UnholyGhast", ghastAspects.copy().add(Aspect.HEAL, 1)));
 
         // SKELETONS
@@ -70,7 +66,7 @@ public class PluginSpecialMobs implements AspectPlugin {
         mobs.add(new ThaumcraftEntity("SpecialMobs.NinjaSkeleton", skeletonAspects.copy().add(Aspect.EXCHANGE, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.PoisonSkeleton", skeletonAspects.copy().add(Aspect.POISON, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.SniperSkeleton", skeletonAspects.copy().add(Aspect.SENSES, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.SpitfireSkeleton", skeletonAspects.copy().add(Aspect.FIRE, 1).add(DarkAspects.NETHER, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.SpitfireSkeleton", skeletonAspects.copy().add(Aspect.FIRE, 2)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.ThiefSkeleton", skeletonAspects.copy().add(Aspect.GREED, 1)));
 
         // SPIDERS
@@ -82,23 +78,23 @@ public class PluginSpecialMobs implements AspectPlugin {
         mobs.add(new ThaumcraftEntity("SpecialMobs.GhostSpider", spiderAspects.copy().add(Aspect.SOUL, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.GiantSpider", new AspectList().add(Aspect.BEAST, 4).add(Aspect.ENTROPY, 3)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.HungrySpider", spiderAspects.copy().add(Aspect.HUNGER, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.MotherSpider", spiderAspects.copy().add(DarkAspects.LUST, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.MotherSpider", spiderAspects.copy().add(Aspect.LIFE, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.PaleSpider", spiderAspects.copy().add(Aspect.POISON, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.PoisonSpider", spiderAspects.copy().add(Aspect.POISON, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.SmallSpider", new AspectList().add(Aspect.BEAST, 2).add(Aspect.ENTROPY, 1).add(Aspect.MOTION, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.ToughSpider", spiderAspects.copy().add(Aspect.LIFE, 1).add(Aspect.ARMOR, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.WebSpider", spiderAspects.copy().add(Aspect.TRAP, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.WitchSpider", spiderAspects.copy().add((Aspect) TC_Aspects.NEBRISUM.mAspect, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.WitchSpider", spiderAspects.copy().add(Aspect.TRAP, 1)));
 
         // CAVE SPIDERS
         AspectList caveSpiderAspects = new AspectList().add(Aspect.BEAST, 3).add(Aspect.ENTROPY, 2).add(Aspect.POISON, 1);
         mobs.add(new ThaumcraftEntity("SpecialMobs.SpecialCaveSpider", caveSpiderAspects));
         mobs.add(new ThaumcraftEntity("SpecialMobs.BabyCaveSpider", new AspectList().add(Aspect.BEAST, 2).add(Aspect.ENTROPY, 1).add(Aspect.POISON, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.FlyingCaveSpider", caveSpiderAspects.copy().add(Aspect.FLIGHT, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.MotherCaveSpider", caveSpiderAspects.copy().add(DarkAspects.LUST, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.MotherCaveSpider", caveSpiderAspects.copy().add(Aspect.LIFE, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.ToughCaveSpider", caveSpiderAspects.copy().add(Aspect.LIFE, 1).add(Aspect.ARMOR, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.WebCaveSpider", caveSpiderAspects.copy().add(Aspect.TRAP, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.WitchCaveSpider", caveSpiderAspects.copy().add((Aspect) TC_Aspects.NEBRISUM.mAspect, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.WitchCaveSpider", caveSpiderAspects.copy().add(Aspect.TRAP, 1)));
 
         // ZOMBIES
         AspectList zombieAspects = new AspectList().add(Aspect.UNDEAD, 2).add(Aspect.MAN, 1).add(Aspect.EARTH, 1);
@@ -133,7 +129,7 @@ public class PluginSpecialMobs implements AspectPlugin {
         AspectList witchAspects = new AspectList().add(Aspect.MAN, 3).add(Aspect.POISON, 1).add(Aspect.MAGIC, 2);
         mobs.add(new ThaumcraftEntity("SpecialMobs.SpecialWitch", witchAspects));
         mobs.add(new ThaumcraftEntity("SpecialMobs.DominationWitch", witchAspects.copy().add(Aspect.DEATH, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.RageWitch", witchAspects.copy().add(DarkAspects.WRATH, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.RageWitch", witchAspects.copy().add(Aspect.ENTROPY, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.ShadowsWitch", witchAspects.copy().add(Aspect.DARKNESS, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.UndeadWitch", witchAspects.copy().add(Aspect.UNDEAD, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.WildsWitch", witchAspects.copy().add(Aspect.BEAST, 1).add(Aspect.LIFE, 1)));
@@ -143,27 +139,27 @@ public class PluginSpecialMobs implements AspectPlugin {
         AspectList blazeAspects = new AspectList().add(Aspect.ELDRITCH, 4).add(Aspect.FIRE, 1);
         mobs.add(new ThaumcraftEntity("SpecialMobs.SpecialBlaze", blazeAspects));
         mobs.add(new ThaumcraftEntity("SpecialMobs.CinderBlaze", new AspectList().add(Aspect.ELDRITCH, 2).add(Aspect.FIRE, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.ConflagrationBlaze", blazeAspects.copy().add(DarkAspects.NETHER, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.EmberBlaze", blazeAspects.copy().add((Aspect) TC_Aspects.STRONTIO.mAspect, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.ConflagrationBlaze", blazeAspects.copy().add(Aspect.FIRE, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.EmberBlaze", blazeAspects.copy().add(Aspect.ENTROPY, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.HellfireBlaze", blazeAspects.copy().add(Aspect.FIRE, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.InfernoBlaze", blazeAspects.copy().add(DarkAspects.WRATH, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.InfernoBlaze", blazeAspects.copy().add(Aspect.FIRE, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.JoltBlaze", blazeAspects.copy().add(Aspect.WEATHER, 1).add(Aspect.TRAVEL, 1)));
         mobs.add(new ThaumcraftEntity("SpecialMobs.SmolderBlaze", blazeAspects.copy().add(Aspect.DARKNESS, 1)));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.WildfireBlaze", blazeAspects.copy().add(DarkAspects.LUST, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.WildfireBlaze", blazeAspects.copy().add(Aspect.LIFE, 1)));
 
         // SLIMES
         AspectList slimeAspects = new AspectList().add(Aspect.WATER, 2).add(Aspect.SLIME, 2);
         mobs.add(new ThaumcraftEntity("SpecialMobs.SpecialSlime", slimeAspects));
-        mobs.add(new ThaumcraftEntity("SpecialMobs.BlackberrySlime", slimeAspects.copy().add(Aspect.ENERGY, 1))); // detonates on targets
-        mobs.add(new ThaumcraftEntity("SpecialMobs.BlueberrySlime", slimeAspects.copy().add(Aspect.WATER, 1))); // moves freely in water, immune to drowning
-        mobs.add(new ThaumcraftEntity("SpecialMobs.CaramelSlime", slimeAspects.copy().add((Aspect) TC_Aspects.MAGNETO.mAspect, 1))); // attaches to targets
-        mobs.add(new ThaumcraftEntity("SpecialMobs.GrapeSlime", slimeAspects.copy().add(Aspect.FLIGHT, 1))); // increased bouncing height, immune to fall damage
-        mobs.add(new ThaumcraftEntity("SpecialMobs.LemonSlime", slimeAspects.copy().add(Aspect.WEATHER, 1).add(Aspect.TRAVEL, 1))); // summons lightnings, teleports on hit
-        mobs.add(new ThaumcraftEntity("SpecialMobs.StrawberrySlime", slimeAspects.copy().add(Aspect.FIRE, 1))); // ignites on hit, immune to fire
-        mobs.add(new ThaumcraftEntity("SpecialMobs.WatermelonSlime", slimeAspects.copy().add(Aspect.MOTION, 1))); // lunges at targets, increased knockback
+        mobs.add(new ThaumcraftEntity("SpecialMobs.BlackberrySlime", slimeAspects.copy().add(Aspect.ENERGY, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.BlueberrySlime", slimeAspects.copy().add(Aspect.WATER, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.CaramelSlime", slimeAspects.copy().add(Aspect.TRAP, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.GrapeSlime", slimeAspects.copy().add(Aspect.FLIGHT, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.LemonSlime", slimeAspects.copy().add(Aspect.WEATHER, 1).add(Aspect.TRAVEL, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.StrawberrySlime", slimeAspects.copy().add(Aspect.FIRE, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.WatermelonSlime", slimeAspects.copy().add(Aspect.MOTION, 1)));
 
         // OTHER MOBS
-        mobs.add(new ThaumcraftEntity("SpecialMobs.LavaMonster", new AspectList().add(Aspect.ELDRITCH, 2).add(Aspect.FIRE, 2).add(DarkAspects.NETHER, 1)));
+        mobs.add(new ThaumcraftEntity("SpecialMobs.LavaMonster", new AspectList().add(Aspect.ELDRITCH, 2).add(Aspect.FIRE, 2)));
 
         return mobs;
     }
